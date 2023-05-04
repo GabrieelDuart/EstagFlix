54fa7a69285dd2a8879e8914b6ac3fa9fae6a3db
GabrielDuarte <2002gabrielduarte@gmail.com>
2023-05-04 12:04:19 -0300

    email php

--- app/paginas/pagina-login/php/conexao-email.php
+++ app/paginas/pagina-login/php/conexao-email.php
@@ -11,7 +11,8 @@ use PHPMailer\PHPMailer\Exception;
 $mail = new PHPMailer(true);
 
 try {
-
+    
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
@@ -19,6 +20,19 @@ try {
     $mail->Passowrd = 'estag@flix4848*';
     $mail->Port = 587;
 
+    $mail->setFrom('estagflix@gmail.com');
+    $mail->addAddress('estagflix@gmail.com');
+    
+    $mail->isHTML(true);
+    $mail->Subject = 'Teste de Email';
+    $mail->Body = 'Isso é apenas um teste <br> Obrigado';
+    $mail->AltBody = 'Isso é apenas um teste';
+
+    if($mail->send()){
+        echo 'Email enviado com sucesso';
+    }else{
+        echo 'Email não enviado';
+    }
     
 
 } catch (Exception $e) {
