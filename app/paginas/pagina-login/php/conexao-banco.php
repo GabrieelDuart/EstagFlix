6be54450b9f785d73a59a8bb177e9989c5bfe063
Gabriel Chagas <biel.silva.chagas@gmail.com>
2023-05-19 15:51:21 -0300

    alterações projeto

--- app/paginas/pagina-login/php/conexao-banco.php
+++ app/paginas/pagina-login/php/conexao-banco.php
@@ -1,16 +1,18 @@
 <?php
 
-$host = getenv('HOST_MYSQL');
-$user = getenv('DB_USER');
-$pass = getenv('DB_PASS');
-$db_name = getenv('DB_NAME');
+#nao apagar
+##//$host = getenv('HOST_MYSQL');
+##//$user = getenv('DB_USER');
+##//$pass = getenv('DB_PASS');
+##//$db_name = getenv('DB_NAME');
 
-define('DB_HOST', $host); 
-define('DB_USER', $user);
-define('DB_PASS', $pass);
-define('DB_NAME', $db_name);
+define('DB_HOST', 'localhost'); 
+define('DB_USER', 'root');
+define('DB_PASS', 'vista');
+define('DB_NAME', 'login');
 
 $conexao = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
 
 
 if (mysqli_connect_errno()) {
